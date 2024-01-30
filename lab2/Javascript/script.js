748334df5fa66e687facf21da387f05e53390467
Mofazzal <mofazzalhossen874@gmail.com>
2024-01-30 14:19:41 +0600

    Flexbox Grid and Javascript files are added

---
+++ lab2/Javascript/script.js
@@ -0,0 +1,6 @@
+function onClicked(){
+            // console.log("Display Button clicked!") ;
+            document.getElementById("heading1").innerHTML = "B1 Group" 
+            document.getElementById("item1").innerHTML = "Web Programming laboratory"
+        }
+
